ac6f656a9f32a93d1b081790925d99cbd1f433ac
Lamont Granquist <lamont@opscode.com>
2013-03-27 11:54:29 -0700

    change from wrapping file provider to wrapping remote_file - gets why-run for free - gets etags / last-modified headers for free - does streaming request to disk instead of blowing out RAM - gains from whatever future features we put into remote_file - BUT, depends on CHEF-4042 since remote_file does not currently take   headers arguments

--- providers/default.rb
+++ providers/default.rb
@@ -1,15 +1,19 @@
-include S3File
+
+def whyrun_supported?
+  true
+end
 
 action :create do
   new_resource = @new_resource
 
-  body = get_from_s3(new_resource.bucket, new_resource.remote_path, new_resource.aws_access_key_id, new_resource.aws_secret_access_key).body
+  s3file = S3UrlGenerator.new(new_resource.bucket, new_resource.remote_path, new_resource.aws_access_key_id, new_resource.aws_secret_access_key)
 
-  file new_resource.path do
+  remote_file new_resource.path do
+    source s3file.url
+    headers s3file.headers
     owner new_resource.owner if new_resource.owner
     group new_resource.group if new_resource.group
     mode new_resource.mode if new_resource.mode
     action :create
-    content body
   end
 end
