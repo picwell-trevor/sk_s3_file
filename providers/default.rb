964a71235f1967f0eb075b32d4e08cbf2c551802
Lamont Granquist <lamont@opscode.com>
2013-03-27 14:04:24 -0700

    support all remote_file actions+attrs

--- providers/default.rb
+++ providers/default.rb
@@ -4,16 +4,35 @@ def whyrun_supported?
 end
 
 action :create do
-  new_resource = @new_resource
+  do_s3_file(:create)
+end
 
+action :create_if_missing do
+  do_s3_file(:create_if_missing)
+end
+
+action :delete do
+  do_s3_file(:delete)
+end
+
+action :touch do
+  do_s3_file(:touch)
+end
+
+def do_s3_file(resource_action)
   s3file = S3UrlGenerator.new(new_resource.bucket, new_resource.remote_path, new_resource.aws_access_key_id, new_resource.aws_secret_access_key)
 
-  remote_file new_resource.path do
+  remote_file new_resource.name do
+    path new_resource.path
+    checksum new_resource.checksum
+    backup new_resource.backup
+    use_etag new_resource.use_etag
+    use_last_modified new_resource.use_last_modified
     source s3file.url
     headers s3file.headers
-    owner new_resource.owner if new_resource.owner
-    group new_resource.group if new_resource.group
-    mode new_resource.mode if new_resource.mode
-    action :create
+    owner new_resource.owner
+    group new_resource.group
+    mode new_resource.mode
+    action resource_action
   end
 end
--- resources/default.rb
+++ resources/default.rb
@@ -7,6 +7,10 @@ attribute :aws_secret_access_key, :kind_of => String
 attribute :owner, :kind_of => [String, NilClass], :default => nil
 attribute :group, :kind_of => [String, NilClass], :default => nil
 attribute :mode, :kind_of => [String, NilClass], :default => nil
+attribute :checksum, :kind_of => [String, NilClass], :default => nil
+attribute :backup, :kind_of => [Integer, FalseClass], :default => 5
+attribute :use_etag, :kind_of => [TrueClass, FalseClass], :default => true
+attribute :use_last_modified, :kind_of => [TrueClass, FalseClass], :default => true
 
 def initialize(*args)
   super
