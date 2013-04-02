9267c5fafa670b8d28c69efffd571640d485a8e2
Lamont Granquist <lamont@opscode.com>
2013-04-02 13:27:10 -0700

    better resource def

--- resources/default.rb
+++ resources/default.rb
@@ -1,11 +1,11 @@
-actions :create
+actions :create, :create_if_missing, :touch, :delete
 attribute :path, :kind_of => String, :name_attribute => true
 attribute :remote_path, :kind_of => String
 attribute :bucket, :kind_of => String
 attribute :aws_access_key_id, :kind_of => String
 attribute :aws_secret_access_key, :kind_of => String
-attribute :owner, :kind_of => [String, NilClass], :default => nil
-attribute :group, :kind_of => [String, NilClass], :default => nil
+attribute :owner, :regex => Chef::Config[:user_valid_regex]
+attribute :group, :regex => Chef::Config[:group_valid_regex]
 attribute :mode, :kind_of => [String, NilClass], :default => nil
 attribute :checksum, :kind_of => [String, NilClass], :default => nil
 attribute :backup, :kind_of => [Integer, FalseClass], :default => 5
@@ -13,6 +13,11 @@ attribute :use_etag, :kind_of => [TrueClass, FalseClass], :default => true
 attribute :use_last_modified, :kind_of => [TrueClass, FalseClass], :default => true
 
 def initialize(*args)
+  version = Chef::Version.new(Chef::VERSION[/^(\d+\.\d+\.\d+)/, 1])
+  if version.major < 10 # || ( version.major == 11 && version.minor < 6 )
+    raise "s3_file provider is not supported on Chef <= 11.6.x"
+  end
   super
   @action = :create
+  @path = name
 end
