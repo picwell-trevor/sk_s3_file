821ce14a76603a633da293cb49b09d543f16d90d
Lamont Granquist <lamont@opscode.com>
2013-07-09 10:41:11 -0700

    feature comptatible with upstream 2.0.4

--- metadata.rb
+++ metadata.rb
@@ -4,4 +4,4 @@ license          "MIT"
 name             "s3_file"
 description      "Installs/Configures s3_file LWRP"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
-version          "1.0"
+version          "2.0.4"
