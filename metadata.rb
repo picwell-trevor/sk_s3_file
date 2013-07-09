3fabc31b278176a971dce82e41a2e3e751201fdf
Lamont Granquist <lamont@opscode.com>
2013-07-09 10:15:46 -0700

    adding metadata.name

--- metadata.rb
+++ metadata.rb
@@ -1,6 +1,7 @@
 maintainer       "YOUR_COMPANY_NAME"
 maintainer_email "brandon.adams@me.com"
 license          "MIT"
+name             "s3_file"
 description      "Installs/Configures s3_file LWRP"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
 version          "1.0"
