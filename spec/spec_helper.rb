cf32e6e45c063c6f50baaebf1b5e53e8b6225247
Lamont Granquist <lamont@scriptkiddie.org>
2014-03-06 19:58:00 -0800

    rubocop fixes

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,7 +3,7 @@ require 'chefspec/berkshelf'
 require 'chefspec/cacher'
 
 RSpec.configure do |config|
-    config.expect_with(:rspec) { |c| c.syntax = :expect }
+  config.expect_with(:rspec) { |c| c.syntax = :expect }
 end
 
 at_exit { ChefSpec::Coverage.report! }
