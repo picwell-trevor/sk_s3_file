62073ef9abc6ee2b4aff058bbaacd44219ca17c5
Lamont Granquist <lamont@scriptkiddie.org>
2014-03-06 16:07:03 -0800

    add more remote_file attrs
    the headers options are merged such that the user could, if they want
    to, override the computed headers...  don't know why, but the user is
    always right no matter where they point the shotgun...

--- providers/default.rb
+++ providers/default.rb
@@ -25,16 +25,20 @@ def do_s3_file(resource_action)
   remote_path = "/#{remote_path}" unless remote_path.chars.first == '/'
   s3file = S3UrlGenerator.new(new_resource.bucket, remote_path, new_resource.aws_access_key_id, new_resource.aws_secret_access_key)
 
+  headers = new_resource.headers.nil? ? s3file.headers : s3file.headers.merge(new_resource.headers)
+
   remote_file new_resource.name do
      path                  new_resource.path
      source                s3file.url
-     headers               s3file.headers
+     headers               headers
      owner                 new_resource.owner unless new_resource.owner.nil?
      group                 new_resource.group unless new_resource.group.nil?
      mode                  new_resource.mode unless new_resource.mode.nil?
      checksum              new_resource.checksum unless new_resource.checksum.nil?
      use_etag              new_resource.use_etag unless new_resource.use_etag.nil?
+     use_etags             new_resource.use_etags unless new_resource.use_etags.nil?
      use_last_modified     new_resource.use_last_modified unless new_resource.use_last_modified.nil?
+     use_conditional_get   new_resource.use_conditional_get unless new_resource.use_conditional_get.nil?
      backup                new_resource.backup unless new_resource.backup.nil?
      inherits              new_resource.inherits unless new_resource.inherits.nil?
      inherits              new_resource.rights unless new_resource.rights.nil?
--- resources/default.rb
+++ resources/default.rb
@@ -12,13 +12,17 @@ attribute :mode, :kind_of => [Integer, String], :default => nil
 attribute :checksum, :kind_of => [String], :default => nil
 attribute :backup, :kind_of => [Integer, FalseClass], :default => nil
 attribute :use_etag, :kind_of => [TrueClass, FalseClass], :default => nil
+attribute :use_etags, :kind_of => [TrueClass, FalseClass], :default => nil
 attribute :use_last_modified, :kind_of => [TrueClass, FalseClass], :default => nil
+attribute :use_conditional_get, :kind_of => [TrueClass, FalseClass], :default => nil
+attribute :headers, :kind_of => Hash, :default => nil
 attribute :sensitive :kind_of => [TrueClass, FalseClass], :default => nil
 attribute :inherits, :kind_of => [TrueClass, FalseClass], :default => nil
 attribute :rights, :kind_of => Hash, :default => nil
 attribute :atomic_update, :kind_of => [TrueClass, FalseClass], :default => nil
 attribute :force_unlink, :kind_of => [TrueClass, FalseClass], :default => nil
 attribute :manage_symlink_source, :kind_of => [TrueClass, FalseClass], :default => nil
+# ftp_active_mode makes no sense with s3 so that attribute of remote_file is ignored
 
 def initialize(*args)
   version = Chef::Version.new(Chef::VERSION[/^(\d+\.\d+\.\d+)/, 1])
