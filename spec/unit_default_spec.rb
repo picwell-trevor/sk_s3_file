8a1e8d7b5ace9c6b534f5299482d6a7bc47f1fba
Lamont Granquist <lamont@scriptkiddie.org>
2014-03-07 16:02:40 -0800

    add tests for header merging

--- spec/unit_default_spec.rb
+++ spec/unit_default_spec.rb
@@ -55,9 +55,35 @@ describe "unit::default" do
     expect(test).to create_remote_file("/tmp/foo.txt").with(source: url)
   end
 
-  it "computes the headers for the remote_file resource" do
-    mock = double(headers: fake_headers, url: url)
-    expect(S3UrlGenerator).to receive(:new).with("mybucket", "/something/foo.txt", "AKIAIISJV5TZ3FPWU3TA", "ABCDEFGHIJKLMNOP1234556/s").and_return(mock)
-    expect(test).to create_remote_file("/tmp/foo.txt").with(headers: fake_headers)
+  context "when testing the header generation with a mock object" do
+    before do
+      mock = double(headers: fake_headers, url: url)
+      expect(S3UrlGenerator).to receive(:new).with("mybucket", "/something/foo.txt", "AKIAIISJV5TZ3FPWU3TA", "ABCDEFGHIJKLMNOP1234556/s").and_return(mock)
+    end
+
+    it "computes the headers for the remote_file resource" do
+      expect(test).to create_remote_file("/tmp/foo.txt").with(headers: fake_headers)
+    end
+
+    context "when overriding the date header" do
+      let(:test) { runner.converge(described_recipe) { runner.find_resource(:sk_s3_file, "/tmp/foo.txt").headers({"date" => "foo"}) } }
+      it "passes the date header to remote_file" do
+        expect(test).to create_remote_file("/tmp/foo.txt").with(headers: fake_headers.merge({"date" => "foo"}))
+      end
+    end
+
+    context "when overriding the authorization header" do
+      let(:test) { runner.converge(described_recipe) { runner.find_resource(:sk_s3_file, "/tmp/foo.txt").headers({"authorization" => "foo"}) } }
+      it "passes the authorization header to remote_file" do
+        expect(test).to create_remote_file("/tmp/foo.txt").with(headers: fake_headers.merge({"authorization" => "foo"}))
+      end
+    end
+
+    context "when adding headers" do
+      let(:test) { runner.converge(described_recipe) { runner.find_resource(:sk_s3_file, "/tmp/foo.txt").headers({"x-foobar" => "foo"}) } }
+      it "merges the header with the computed headers" do
+        expect(test).to create_remote_file("/tmp/foo.txt").with(headers: fake_headers.merge({"x-foobar" => "foo"}))
+      end
+    end
   end
 end
